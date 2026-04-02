2e5500daf1be4143d6c74d4269ca34baf5d5dd03
Francesco Maria Tripodi <fm.tripodi@almaviva.it>
2026-04-03 00:11:30 +0200

    pp

--- src/main/java/com/qtm/tenants/TenantsAppApplication.java
+++ src/main/java/com/qtm/tenants/TenantsAppApplication.java
@@ -2,7 +2,7 @@ package com.qtm.tenants;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
 /**
  * Entry point dell'applicazione tenants-app.
  */
