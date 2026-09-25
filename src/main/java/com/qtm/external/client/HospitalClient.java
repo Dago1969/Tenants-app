2463780b3ec09d8243a588ea01972313f7dca335
Dago1969 <francesco.tripodi@gmail.com>
2026-09-25 09:26:41 +0200

    modifiche vecchi servizi

--- src/main/java/com/qtm/external/client/HospitalClient.java
+++ src/main/java/com/qtm/external/client/HospitalClient.java
@@ -1,4 +1,4 @@
-package com.qtm.dashboard.client;
+package com.qtm.external.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
